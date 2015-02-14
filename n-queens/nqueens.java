c4dbcbe325d60e644d36721e43534096928f74e4
josephhan11 <sjhan123@gmail.com>
2015-02-13 21:33:05 -0500

    clean some junk

--- n-queens/nqueens.java
+++ n-queens/nqueens.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class nqueens {
 
-    //--------------------------------
     private char[][] board;
     private int side, maxX, maxY;
     private char taken = '.';
@@ -15,7 +14,6 @@ public class nqueens {
 	    Thread.sleep(n);
 	} catch (Exception e) {}
     }
-    //--------------------------------
 
     public nqueens(int a) {
 	side = a;
@@ -35,18 +33,14 @@ public class nqueens {
     public String toString(){
 	String s = "[2J\n";
 	s+="+";
-	for (int y=0;y<2*maxY+1;y++){
-	    s += "-";}
+	for (int y=0;y<2*maxY+1;y++){s += "-";}
 	s+="+ \n";
 	for (int y=0;y<maxY;y++){
 	    s+="| ";
-	    for (int x=0;x<maxX;x++)
-		s = s +board[x][y] + " ";
-	    s=s+"| \n";
-	    }
+	    for (int x=0;x<maxX;x++){s = s +board[x][y] + " ";}
+	    s=s+"| \n";}
 	s+="+";
-        for (int y=0;y<2*maxY+1;y++){
-            s += "-";}
+        for (int y=0;y<2*maxY+1;y++){s += "-";}
         s+="+ \n";
 	return s;
     }
@@ -62,40 +56,33 @@ public class nqueens {
 
     public void taker(int spX, int spY){
 	for(int a=0; a<side;a++){
-	    if (board[spX][a] != queen){
-		board[spX][a] = taken;
-	    }
-	    if (board[a][spY] != queen){
-		board[a][spY] = taken;}
+	    if (board[spX][a] != queen){board[spX][a] = taken; }
+	    if (board[a][spY] != queen){board[a][spY] = taken;}
 	}
        	for(int r=0; r<side;r++){
 	    for(int s=0; s<side;s++){
 		if((spX-r) == (spY-s)){board[r][s]=taken;}
 		if((spY-s) == (s-spY)){board[r][s]=taken;}
-		board[spX][spY]=queen;
-		}
+		board[spX][spY]=queen;}
 	    }
     }
 
-
     public void solve(){
-
 	//PLOTS A QUEEN ---------------
 	Random r = new Random();
 	int spotX = r.nextInt(side);
         int spotY = r.nextInt(side);
-	if (board[spotX][spotY] == space){
-	    board[spotX][spotY] = queen;
+	if (board[spotX][spotY] == space){board[spotX][spotY] = queen;
 	//FINDS IMPOSSIBLE PLACES -----
 	    taker(spotX,spotY);	
-	    //    System.out.println(this);
-	    // delay(250);
+	    System.out.println(this);
+	    delay(100);
 	}
 	if (checker() == true){solve();}
     }
 
     public static void main(String[] args){
-	nqueens q = new nqueens(15);
+	nqueens q = new nqueens(20);
 	System.out.println(q);
 	q.solve();
 	System.out.println(q);
