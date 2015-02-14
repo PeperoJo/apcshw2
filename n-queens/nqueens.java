ace3925d341b8523c92032638d56aa4870bb1992
josephhan11 <sjhan123@gmail.com>
2015-02-13 20:31:16 -0500

    almost done

--- n-queens/nqueens.java
+++ n-queens/nqueens.java
@@ -5,8 +5,8 @@ public class nqueens {
 
     //--------------------------------
     private char[][] board;
-    private int maxX, maxY;
-
+    private int side, maxX, maxY;
+    private char taken = '.';
     private char space = 'o';
     private char queen = 'Q';
 
@@ -18,9 +18,9 @@ public class nqueens {
     //--------------------------------
 
     public nqueens() {
-
-	maxX = 5;
-	maxY = 5;
+	side = 5;
+	maxX = side;
+	maxY = side;
 	board = new char[maxX][maxY];
 	filler();
     }
@@ -38,14 +38,63 @@ public class nqueens {
 	for (int y=0;y<maxY;y++)
 	    {
 		for (int x=0;x<maxX;x++)
-		    s = s +board[x][y];
+		    s = s +board[x][y] + " ";
 		s=s+"\n";
 	    }
 	return s;
     }
 
+    public boolean checker(){
+	for(int a=0;a<side;a++){
+	    for(int b=0;b<side;b++){
+		if (board[a][b] == space){return true;}
+	    }
+	}
+	return false;
+    }
+
+    public void taker(int spX, int spY){
+	for(int a=0; a<side;a++){
+	    if (board[spX][a] != queen){
+		board[spX][a] = taken;
+	    }
+	    if (board[a][spY] != queen){
+		board[a][spY] = taken;}
+	}
+	/*	for(int r=-1*side; r<side;r++){
+	    for(int s=-1*side; s<side;s++){
+		
+		while (((spX+r)>=0) && 
+		       ((spX+r)<=side) &&
+		       ((spY+s)>=0) &&
+		       ((spY+s)<=side)){
+		    if (board[spX+r][spY+s] != queen){
+			board[spX+r][spY+s] = taken;}
+		}
+	    }
+	    }*/
+    }
+
+
+    public void solve(){
+
+	//PLOTS A QUEEN ---------------
+	Random r = new Random();
+	int spotX = r.nextInt(side);
+        int spotY = r.nextInt(side);
+	if (board[spotX][spotY] == space){
+	    board[spotX][spotY] = queen;
+	    //FINDS IMPOSSIBLE PLACES -
+	    taker(spotX,spotY);	
+	    System.out.println(this);
+	    delay(250);
+	}
+	if (checker() == true){solve();}
+    }
+
     public static void main(String[] args){
 	nqueens q = new nqueens();
 	System.out.println(q);
+	q.solve();
     }
 }
