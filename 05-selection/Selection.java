36d639b05a70e4652f35395e2c08d244e2062392
josephhan11 <sjhan123@gmail.com>
2015-04-12 16:23:30 -0400

    old hw

---
+++ 05-selection/Selection.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Selection {
+
+    public int split( int[] a, int l, int h ) {
+	int temp;
+	int pi = 1;
+	int pivot = a[pi];
+	int wall = 1;
+	for (int i=0;i<a.length;i++) {
+	    if ( A[i] > pivot ) {
+		temp = a[i];
+		a[i] = a[wall];
+		a[wall] = temp;
+		wall++;
+	    }
+	}
+	if ( temp[k] == pivot ) {
+	    return temp[k];
+	}
+	return temp[k];
+    }
+
+    public static void main(String[] args) {
+	int[] a = { 1, 0, 9, 8, 5};
+	Selection s = new Selection();
+	System.out.println(s.select(3,a, 0, a.length - 1));
+    }
+
+}
