cc0a6046538e051c92343b5b5a237fd8a11d24f3
josephhan11 <sjhan123@gmail.com>
2015-04-20 01:29:18 -0400

    longlonglonghw

---
+++ 13-search/Frontier.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Frontier {
+    public LinkedList<Node> l = new LinkedList<Node>();
+
+    public void add(Node n){
+	int i =0;
+	for (Node tmp : l){
+	    if(tmp.getPriority()<n.getPriority()){
+		i++;
+	    }
+	}
+	l.add(i,n);
+    }
+
+    public Node remove() {
+	return l.remove(0);
+    }
+        
+    public boolean isEmpty(){
+	return l.isEmpty();
+    }
+}
+
+
---
+++ 13-search/Node.java
@@ -0,0 +1,45 @@
+public class Node {
+    private int x,y;
+    private Node prev;
+    private double priority;
+    private int step;
+    
+    public Node(int x, int y){
+	this.x = x;
+	this.y = y;;
+    }
+
+    public double getPriority(){
+	return priority;
+    }
+    
+    public void setPriority(double p){
+	priority = p;
+    }
+
+    public int getStep(){
+	return step;
+    }
+    
+    public void setStep(int s){
+	step=s;
+    }
+    
+    public Node getPrev() {
+	return prev;
+    }
+
+    public void setPrev(Node n){
+	prev = n;
+    }
+    
+    public int getX() {
+	return x;
+    }
+
+    public int getY() {
+	return y;
+    }
+    
+}
+
